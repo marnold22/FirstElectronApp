d202f0e5f27c0a1bf631aeb622c88ab45c1489b2
Michael <minimearnold@gmail.com>
2020-02-26 09:57:30 -0800

    addWindow function & lables for menu
    Created Addwindow inital function, and created labels for the menu system

--- main.js
+++ main.js
@@ -2,9 +2,10 @@ const electron = require('electron');
 const url = require('url');
 const path = require('path');
 
-const {app, BrowserWindow} = electron;
+const {app, BrowserWindow, Menu} = electron;
 
 let mainWindow;
+let addWindow;
 
 // Listen for app to bne ready
 app.on('ready', function() {
@@ -17,4 +18,52 @@ app.on('ready', function() {
     protocol: 'file:',
     slashes: true
   }));
+
+  // Build Menu from template
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+
+  // insert the menu
+  Menu.setApplicationMenu(mainMenu);
 });
+
+// Handle createAddWindow
+function createAddWindow() {
+  // Create new window
+  addWindow = new BrowserWindow({
+    width: 300,
+    height: 200,
+    title: 'Add Shopping List Item'
+  });
+
+  //Load html into window
+  addWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'addWindow.html'),
+    protocol: 'file:',
+    slashes: true
+  }));
+}
+
+// Create menu template
+const mainMenuTemplate = [
+  {
+    label:'File',
+    submenu:[
+      {
+        label: 'Add Item',
+        click(){
+          createAddWindow();
+        }
+      },
+      {
+        label: 'Clear Items'
+      },
+      {
+        label: 'Quit',
+        accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+        click(){
+          app.quit();
+        }
+      }
+    ]
+  }
+];
