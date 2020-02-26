e0e5e23a338fab6cb56b2b9269aabd7cf6782020
Michael <minimearnold@gmail.com>
2020-02-26 10:37:12 -0800

    Added clear items functionality

--- main.js
+++ main.js
@@ -80,7 +80,10 @@ const mainMenuTemplate = [
         }
       },
       {
-        label: 'Clear Items'
+        label: 'Clear Items',
+        click(){
+          mainWindow.webContents.send('item:clear');
+        }
       },
       {
         label: 'Quit',
