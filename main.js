d27f715c8c79d8c8b364779b3725f7bc6a324fc2
Michael <minimearnold@gmail.com>
2020-02-26 10:10:44 -0800

    Created devtools and added to main menu

--- main.js
+++ main.js
@@ -85,3 +85,22 @@ const mainMenuTemplate = [
 if(process.platform == 'darwin'){
   mainMenuTemplate.unshift({});
 }
+
+// Add developer tools item if not in production
+if(process.env.NODE_ENV !== 'production'){
+  mainMenuTemplate.push({
+    label: 'Developer Tools',
+    submenu:[
+      {
+        label: 'Toggle Dev Tools',
+        accelerator: process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
+        click(item, focusedWindow){
+          focusedWindow.toggleDevTools();
+        }
+      },
+      {
+        role: 'reload'
+      }
+    ]
+  });
+}
